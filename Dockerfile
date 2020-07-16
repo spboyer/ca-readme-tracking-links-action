8131a7058e87b58cc16c7370ed52cc5051f7b20b
Shayne Boyer <spboyer@live.com>
2020-07-16 09:27:45 -0400

    feat: Inital Action

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM mcr.microsoft.com/dotnet/core/runtime:3.1-buster-slim AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster AS build
+WORKDIR /src
+COPY ["app/readme-link-trends.csproj", ""]
+RUN dotnet restore "./readme-link-trends.csproj"
+COPY app/ .
+WORKDIR "/src/."
+RUN dotnet build "readme-link-trends.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "readme-link-trends.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "readme-link-trends.dll"]
---
+++ app/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace readme_link_trends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessReadMe();
+        }
+
+        static void ProcessReadMe()
+        {
+            var readme = File.ReadAllText("/github/home/README.md");
+            
+            var pattern = @"(((http|ftp|https):\/\/)?[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?)";
+            Regex regex = new Regex(pattern);
+
+            string result = regex.Replace(readme, m => Tracking.AppendTrackingInfo(m.Groups[0].Value, "regex", "github", "shboyer"));
+
+            File.WriteAllText("/github/home/README.md", result);
+        }
+
+    }
+}
